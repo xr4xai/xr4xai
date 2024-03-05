cfc39e23b26f1eab24a1d10f8889502850a989e7
6a6c <jlooney6@vols.utk.edu>
2024-03-04 23:00:35 -0500

    Added test snn network, and spikes that move along edges #1 #3

--- graphics/qt_main.py
+++ graphics/qt_main.py
@@ -1,12 +1,51 @@
 import sys
 from typing import Iterable
-from PyQt6.QtWidgets import QGraphicsScene, QGraphicsSceneDragDropEvent, QGraphicsSceneHoverEvent, QGraphicsSceneMouseEvent, QGraphicsView, QGraphicsItem, QGraphicsRectItem, QGraphicsEllipseItem, QApplication, QPushButton, QGraphicsPathItem
-from PyQt6.QtGui import QBrush, QDragEnterEvent, QMouseEvent, QPen, QColor, QDrag, QPainterPath, QGradient, QRadialGradient, QLinearGradient
-from PyQt6.QtCore import QRectF, Qt, QEvent, QObject, QLineF, QMimeData
+from PyQt6.QtWidgets import (
+    QGraphicsScene, 
+    QGraphicsSceneDragDropEvent, 
+    QGraphicsSceneHoverEvent, 
+    QGraphicsSceneMouseEvent, 
+    QGraphicsView, 
+    QGraphicsItem, 
+    QGraphicsRectItem, 
+    QGraphicsEllipseItem, 
+    QApplication, 
+    QPushButton, 
+    QGraphicsPathItem,
+    QSlider,
+    )
+from PyQt6.QtGui import (
+    QBrush, 
+    QDragEnterEvent, 
+    QMouseEvent, 
+    QPen, 
+    QColor, 
+    QDrag, 
+    QPainterPath, 
+    QGradient, 
+    QRadialGradient, 
+    QLinearGradient,
+    )
+from PyQt6.QtCore import (
+    QRectF, 
+    Qt, 
+    QEvent, 
+    QObject, 
+    QLineF, 
+    QMimeData,
+    QPointF
+    )
+
+import os
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/../snn" )
+import test_network
+import math
+
+# Global value visual_time repesents the time in the simulations currently shown
+# It will get updated by the GraphicsView
+visual_time = -1
 
 
-# Defining a scene rect of 400x200, with it's origin at 0,0.
-# If we don't set this on creation, we can set it later with .setSceneRect
 class Node(QGraphicsEllipseItem):
     def __init__(self, parent, x, y, curId, nodeType, edgeFunction):
         self.parent = parent
@@ -63,9 +102,13 @@ class Node(QGraphicsEllipseItem):
 
 
 class Edge(QGraphicsPathItem):
-    def __init__(self, source, sink):
+    def __init__(self, source, sink, weight = 1, delay = 1):
         self.sourceNode = source
         self.sinkNode = sink
+        self.weight = weight
+        self.delay = delay
+
+        self.spike_vec = []
 
         super().__init__()
 
@@ -80,8 +123,6 @@ class Edge(QGraphicsPathItem):
 
 
     def update(self):
-        print(self)
-
         self.path.clear()
 
         self.draw_edge()
@@ -96,6 +137,33 @@ class Edge(QGraphicsPathItem):
         self.path.quadTo(self.sinkNode.pos().x() -  3, self.sinkNode.pos().y() - 3, self.sinkNode.pos().x() - 3, self.sinkNode.pos().y() - 3 )
         self.path.quadTo(self.sourceNode.pos().x() - 3, self.sourceNode.pos().y() - 3, self.sourceNode.pos().x() - 3, self.sourceNode.pos().y() - 3 )
         self.path.quadTo(self.sourceNode.pos().x() + 3, self.sourceNode.pos().y() + 3, self.sourceNode.pos().x() + 3, self.sourceNode.pos().y() + 3)
+        
+        elapsed_spikes = []
+        for spike in self.spike_vec:
+            if spike > visual_time:
+                break
+            elapsed_spikes.append(spike)
+
+        for most_recent_spike in elapsed_spikes:
+
+            distance_along_edge = (visual_time - most_recent_spike) / self.delay
+            print(distance_along_edge)
+            if distance_along_edge < 0  or distance_along_edge > 1 :
+                continue
+
+            offx = (self.sinkNode.pos().x() - self.sourceNode.pos().x() ) 
+            offx /= math.sqrt((self.sinkNode.pos().y() - self.sourceNode.pos().y() ) ** 2 + (self.sinkNode.pos().x() - self.sourceNode.pos().x()) ** 2) 
+            offx *= self.sourceNode.r
+            offy = (self.sinkNode.pos().y() - self.sourceNode.pos().y() )
+            offy /= math.sqrt((self.sinkNode.pos().y() - self.sourceNode.pos().y() ) ** 2 + (self.sinkNode.pos().x() - self.sourceNode.pos().x()) ** 2)
+            offy *= self.sourceNode.r
+            cx = offx + self.sourceNode.pos().x() + distance_along_edge * ( self.sinkNode.pos().x() - self.sourceNode.pos().x() )
+            cy = offy + self.sourceNode.pos().y() + distance_along_edge * ( self.sinkNode.pos().y() - self.sourceNode.pos().y() )
+
+            print(f"{self.sourceNode.id}->{self.sinkNode.id} is drawing a spike at {cx} {cy}. dist_along = {distance_along_edge}")
+
+            self.path.addEllipse(QPointF(cx, cy) , 15, 15)
+
 
 
 
@@ -103,7 +171,8 @@ class AGraphicsView(QGraphicsView):
     def __init__(self, scene: QGraphicsScene):
         super().__init__(scene)
         self.scene = scene
-
+        
+        # Add Node Button
         button = QPushButton("Add Node")
         button.resize(750, 75)
         button.move(25, 500)
@@ -113,11 +182,48 @@ class AGraphicsView(QGraphicsView):
 
         self.scene.addWidget(button)
 
+        # Add Slider for time
+        time_slider = QSlider(Qt.Orientation.Horizontal, self)
+        time_slider.setRange(0, 100)
+        visual_time = 0
+        time_slider.setValue(0)
+        time_slider.move(25, 25)
+
+        time_slider.valueChanged.connect(self.updateSimTimeFromSlider)
+        
+        self.scene.addWidget(time_slider)
+
         self.curId = 0
         self.nodeIds = []
         self.dictOfEdges = {}
-        self.dictOfEdgeItems = {}
         self.edgeBuf = []
+    
+        self.createTestNetwork()
+        self.spike_vec = test_network.test_net_spike_vec()
+
+
+    def createTestNetwork(self):
+        n1 = Node(self, 100, 300, 0, "input", self.connectNodes)
+        n2 = Node(self, 400, 150, 1, "output", self.connectNodes)
+        n3 = Node(self, 400, 300, 2, "output", self.connectNodes)
+        n4 = Node(self, 400, 450, 3, "output", self.connectNodes)
+
+        self.scene.addItem(n1); self.scene.addItem(n2);
+        self.scene.addItem(n3); self.scene.addItem(n4);
+    
+        # We have to write some new methods for this
+        self.connectNodes(n1, 0); self.connectNodes(n2, 1);
+        self.connectNodes(n1, 0); self.connectNodes(n3, 2);
+        self.connectNodes(n1, 0); self.connectNodes(n4, 3);
+            
+        self.dictOfEdges["0->2"].delay = 3
+
+    def updateSimTimeFromSlider(self, val):
+        global visual_time
+        visual_time = val / 10
+        print(visual_time)
+
+        self.updateEdges()
 
     def mousePressEvent(self, event: QMouseEvent) -> None:
         if self.addNode == True:
@@ -161,9 +267,13 @@ class AGraphicsView(QGraphicsView):
             self.edgeBuf.append([id, obj])
 
     def updateEdges(self):
-        
+
         for e in self.dictOfEdges:
             #self.scene.removeItem(self.dictOfEdgeItems[e])
+            
+            
+            self.dictOfEdges[e].spike_vec = self.spike_vec[ self.dictOfEdges[e].sourceNode.id ]
+
             self.dictOfEdges[e].update()
 
 
